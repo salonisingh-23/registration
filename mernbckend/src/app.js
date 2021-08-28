10f0ccccf0d065a2e15ef47a671fb2c5bdad37f3
salonisingh-23 <salonisingh5723@gmail.com>
2021-08-28 12:33:57 +0530

    changes updated

--- mernbckend/src/app.js
+++ mernbckend/src/app.js
@@ -26,6 +26,55 @@ res.render("index")
 
 app.get("/register",(req,res)=>{
     res.render("register");
+    // console.log(`${email}`);
+    // console.log(req.body.pass);
+})
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+app.get("/forgot",(req,res)=>{
+    res.render("forgot");
+})
+app.get("/home",(req,res)=>{
+    res.render("home");
+})
+app.post('/forgot',async(req,res)=>{
+    try{
+    const email=req.body.email;
+    const newpass=req.body.newpass;
+    const cpass=req.body.cpass;
+    //const usermail=await Register.findOne({email:email});
+    if(newpass===cpass){
+    await Register.findOneAndUpdate({email:email},{$set:{pass:newpass}});
+    res.render("index");
+   }
+   else{
+       res.send("Passwords don't match");
+   }
+    }
+    catch(error){
+        console.log(error);
+    }
+})
+app.post("/login",async(req,res)=>{
+    try{
+        const email=req.body.email;
+        const pass=req.body.pass;
+        // console.log(`${email} and password is ${password}`)
+        const useremail=await Register.findOne({email:email});
+        if(useremail.pass=== pass)
+        {
+            res.status(201).render("home");
+        }
+        else{
+            res.send("Password entered is wrong");
+        }
+        // res.send(useremail);
+        // console.log(useremail);
+    }
+   catch(error){
+       res.status(400).send("Invalid Email");
+   }
 })
 
 app.post("/register",async(req,res)=>{
@@ -33,7 +82,7 @@ app.post("/register",async(req,res)=>{
      
 const registerEmployee=new Register({
     email: req.body.email,
-    pass: req.body.pass
+    pass: req.body.pass 
 })
 const registered=await registerEmployee.save();
 res.status(201).render("index");
